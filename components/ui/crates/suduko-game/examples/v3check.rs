4fe0b178849998a75c21e7c9769964802183d36c
Mike Wright <mike@softwarewrighter.com>
2026-08-25 09:33:34 -0700

    chore: drop debug codec example; step 024 metadata

--- components/ui/crates/suduko-game/examples/v3check.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let code = suduko_game::save(0, None, &std::collections::BTreeMap::new());
-    println!("code: {code}");
-    println!("fields: {}", code.split('|').count());
-    println!(
-        "restore: {:?}",
-        suduko_game::restore(&code).map(|s| (s.level, s.stats, s.game.is_some()))
-    );
-}
